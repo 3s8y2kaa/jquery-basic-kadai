03daa02b9ddde5678271f23e953e84afff31cc0c
takada sayaka <140459396+3s8y2kaa@users.noreply.github.com>
2023-09-13 07:58:52 +0900

    課題10提出

---
+++ kadai_010/summary.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('#chage-color').on('click', function(){
+    $('#target').css('color', 'red');
+  });
+  $('#change-text').on('click', function() {
+    $('#target').text('Hello!');
+  });
+  $('#fade-out').on('click', function() {
+    // フェードアウトする
+    $('#target').fadeOut();
+  });
+  $('#fade-in').on('click', function() {
+    // フェードアウトする
+    $('#target').fadeIn();
+  });
+});
